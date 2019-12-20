e0ec980ba05db495cc31ba178109c669615facab
shahimvedaei <53317554+shahimvedaei@users.noreply.github.com>
2019-12-19 23:58:54 -0600

    Update RndGenerator.py

--- RndGenerator.py
+++ RndGenerator.py
@@ -1,8 +1,15 @@
 '''
 About:
-This program generates N random number which is between min-max value
-and the total average is avg
+This program generates N random numbers which are between min-max values and the total average is avg
 Note: all numbers with 2 Decimal precision
+
+Inputs: avg: What is the average for the numbers
+min: What is the minimum number
+max: What is the maximum number
+N: Howmany numbers do you want to generate
+Outputs: average: The computed average for the generated number
+Because the decimal precision sets to 2, there is a small variation from input avg 
+arr: This array contained the values of the random number.
 ------------------------------------------------------------------------
 Author: Shahim Vedaei
 Last modification: Dec 19 2019
